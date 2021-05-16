d23b8bcc8587bf79649088e5172170cad01f5bd5
Shu Miyao <shu.andrade.miyao@gmail.com>
2021-05-16 20:43:13 +0100

    Update. Project id 6

--- assets/js/components.js
+++ assets/js/components.js
@@ -1,5 +1,5 @@
 // Components
-Vue.component('Layout', {
+Vue.component("Layout", {
   template: decodeURIComponent(siteData.template_parts.layout.template),
   data() {
     return {
@@ -8,11 +8,11 @@ Vue.component('Layout', {
   },
 });
 
-Vue.component('Header', {
+Vue.component("ContentBody", {
   template: decodeURIComponent(siteData.template_parts.header.template),
   data() {
     return {
-      showMenu: false
+      showMenu: false,
     };
   },
   methods: {
@@ -33,12 +33,41 @@ Vue.component('Header', {
   },
 });
 
-Vue.component('Blocks', {
+// Section template
+
+// block template
+
+Vue.component("Header", {
+  template: decodeURIComponent(siteData.template_parts.header.template),
+  data() {
+    return {
+      showMenu: false,
+    };
+  },
+  methods: {
+    async directToPage(pageID) {
+      // redirect to the next page
+      const _pageSlug = this.getPageSlug(pageID);
+      // / for homepage (root top)
+      await this.$router.push(
+        { path: _pageSlug || "/" },
+        // onComplete
+        () => {
+          this.pageUpdatedSequencer(pageID);
+        },
+        // onAbort
+        (error) => error
+      );
+    },
+  },
+});
+
+Vue.component("Blocks", {
   template: decodeURIComponent(siteData.template_parts.blocks.template),
   data() {
     return {
       wrapperClass: "flex-1",
-      filter: "all"
+      filter: "all",
     };
   },
   methods: {
@@ -51,16 +80,15 @@ Vue.component('Blocks', {
       // No need for filter because there is not any item.
       if (gridItems.length === 0) return false;
       console.log(gridItems);
-      const _listOfItemTypes = [
-        ...new Set(gridItems.map(item => item.type))
-      ];
+      const _listOfItemTypes = [...new Set(gridItems.map((item) => item.type))];
       // Return an array with item types.
-      if (_listOfItemTypes.length > 1)
-        return ["all", ..._listOfItemTypes];
+      if (_listOfItemTypes.length > 1) return ["all", ..._listOfItemTypes];
       // No need to show filter because there is only one type of items.
       return false;
-    }
-  }
+    },
+  },
 });
 
-Vue.component('Footer', { template: decodeURIComponent(siteData.template_parts.footer.template) });
+Vue.component("Footer", {
+  template: decodeURIComponent(siteData.template_parts.footer.template),
+});
